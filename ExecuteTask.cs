33a98165901b20e57f38373d89f8280a4bd92a52
fabio <fabio@DESKTOP-O3D193T>
2020-01-01 23:40:23 -0300

    Task

---
+++ ExecuteTask.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Author: Fábio Said
+ * Date: 21/12/2019 23:10
+ * 
+ */
+
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace CAP1
+{
+    public class ExecuteTask
+    {
+        public static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Parallel.Invoke(() => Task.First(), () => Task.Second(), () => Task.Third());
+            stopwatch.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine(" Task Completed! {0}", stopwatch.ElapsedMilliseconds / 1000.0);
+
+            stopwatch.Reset();
+            stopwatch.Start();
+            Task.First();
+            Task.Second();
+            Task.Third();
+            stopwatch.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine(" Task Completed! {0}", stopwatch.ElapsedMilliseconds / 1000.0);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task.cs
@@ -0,0 +1,46 @@
+﻿/*
+ * Author: Fábio Said
+ * Date: 21/12/2019 23:10
+ * 
+ */
+
+using System.Threading;
+using System;
+
+namespace CAP1
+{
+    public class Task
+    {
+        public static void First()
+        {
+            for (int count = 0; count <= 300; count++)
+            {
+                Time();
+                Console.Write("1");
+            }
+        }
+
+        public static void Second()
+        {
+            for (int count = 0; count <= 300; count++)
+            {
+                Time();
+                Console.Write("2");
+            }
+        }
+
+        public static void Third()
+        {
+            for (int count = 0; count <= 300; count++)
+            {
+                Time();
+                Console.Write("3");
+            }
+        }
+
+        private static void Time()
+        {
+            Thread.Sleep(10);
+        }
+    }
+}
